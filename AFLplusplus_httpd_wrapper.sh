02857304e929d28988ac607c3e151f7139ee1ef9
Jacob Hoopes <jake.hoopes@gmail.com>
2020-08-25 13:55:23 -0600

    Begin instrumentation support for Apache modules

--- AFLplusplus_httpd_wrapper.sh
+++ AFLplusplus_httpd_wrapper.sh
@@ -3,24 +3,49 @@ usage() {
   echo 'USAGE:'
   echo "${0}
     -h                    # Display USAGE
-    -m <master || slave>  # afl++ Mode
-    -a <mod_auth_pam,...> # Comma-delimited httpd Modules to Instrument
-    -n                    # Nuke contents of multi-sync (New afl++ Session)
+
+    -m <master || slave>  # REQUIRED
+                          # afl++ Mode 
+
+    -a <mod_auth_pam,...> # OPTIONAL / master MODE ONLY
+                          # Comma-delimited httpd Modules to Instrument
+
+    -n                    # OPTIONAL
+                          # Nuke contents of multi-sync (New afl++ Session)
+
+    -c <httpd.conf>       # OPTIONAL
+                          # Path to custom httpd.conf file
+                          # for more advanced httpd mockups
+
+    -L                    # OPTIONAL
+                          # List supported httpd modules to instrument
   "
   exit 1
 }
 
+list_supported_httpd_modules_to_instrument() {
+  echo "List of Supported httpd Modules to Instrument:
+    mod_auth_pam,
+    mod_fastcgi,
+    mod_ssl
+  "
+  exit 0
+}
+
 no_args='true'
+custom_httpd_conf=''
 afl_mode=''
 httpd_modules_for_instrumentation=''
 nuke_multi_sync='false'
 
-while getopts ":hm:a:n" flag; do
-  case "${flag}" in
+while getopts "hc:m:a:nL" flag; do
+  case $flag in
     'h') usage;;
     'm') afl_mode="${OPTARG}";;
     'a') httpd_modules_for_instrumentation="${OPTARG}";;
     'n') nuke_multi_sync='true';;
+    'f') custom_httpd_conf="${OPTARG}";;
+    'L') list_supported_httpd_modules_to_instrument;;
     *) usage;;
   esac
   no_args='false'
@@ -36,15 +61,19 @@ repo_name=`basename ${repo_root}`
 docker_repo_root="/opt/${repo_name}"
 
 fuzz_session_root='/fuzz_session'
-test_case_root="${docker_repo_root}/test_cases"
-httpd_test_cases="${test_case_root}/httpd"
+httpd_test_cases="${docker_repo_root}/httpd/test_cases"
 
 afl_session_root="${fuzz_session_root}/AFLplusplus"
 afl_input="${afl_session_root}/input"
 afl_output="${afl_session_root}/multi_sync"
 
 httpd_repo="${fuzz_session_root}/httpd"
-target_binary="${httpd_repo}/BINROOT/bin/httpd -X"
+
+if [[ $custom_httpd_conf == '' ]]; then
+  target_binary="${httpd_repo}/BINROOT/bin/httpd -X"
+else
+  target_binary="${httpd_repo}/BINROOT/bin/httpd -X"
+fi
 
 if [[ ! -d $afl_session_root ]]; then
   mkdir $afl_session_root
@@ -81,23 +110,42 @@ fuzz_session_init="
 
 case $afl_mode in
   'master')
+    # Build out afl_instrument_httpd_and_fuzz_session_init 
+    # by parsing httpd_modules_for_instrumentation
     echo 'Building latest trunk of httpd...'
-    afl_instrument_httpd="${docker_repo_root}/httpd_instrument_w_aflplusplus.sh"
+    afl_instrument_httpd="${docker_repo_root}/httpd/httpd_instrument_w_aflplusplus.sh"
+    afl_instrument_httpd_and_fuzz_session_init="${afl_instrument_httpd} &&"
 
-    # TODO: Choose if we want to incorporate additional 
-    # Apache modules into fuzzing session
-    afl_instrument_mod_auth_pam="${docker_repo_root}/mod_auth_pam/mod_auth_pam_instrument_w_aflplusplus.sh"
-
-    #afl_instrument_httpd_and_fuzz_session_init="
-    #  ${afl_instrument_httpd} && 
-    #  ${afl_instrument_mod_auth_pam} && 
-    #  ${fuzz_session_init}
-    #"
-    
-    afl_instrument_httpd_and_fuzz_session_init="
-      ${afl_instrument_httpd} && 
-      ${fuzz_session_init}
-    "
+    delimit=',' read -r -a httpd_mod_arr <<< "$httpd_modules_for_instrumentation"
+    for httpd_module in "${httpd_mod_arr[@]}"; do
+      case $httpd_module in 
+        'mod_auth_pam')
+          echo "Instrumenting ${httpd_module}!"
+          mod_auth_pam_test_cases="${docker_repo_root}/mod_auth_pam/test_cases"
+          cp $mod_auth_pam_test_cases/* $afl_input
+          afl_instrument_mod_auth_pam="${docker_repo_root}/mod_auth_pam/mod_auth_pam_instrument_w_aflplusplus.sh"
+          afl_instrument_httpd_and_fuzz_session_init="${afl_instrument_mod_auth_pam} &&"
+          ;;
+        'mod_fastcgi') 
+          echo "Instrumenting ${httpd_module}!"
+          mod_fastcgi_test_cases="${docker_repo_root}/mod_fastcgi/test_cases"
+          cp $mod_fastcgi_test_cases/* $afl_input
+          afl_instrument_mod_fastcgi="${docker_repo_root}/mod_fastcgi/mod_fastcgi_instrument_w_aflplusplus.sh"
+          afl_instrument_httpd_and_fuzz_session_init="${afl_instrument_mod_fastcgi} &&"
+          ;;
+        'mod_ssl')
+          echo "Instrumenting ${httpd_module}!"
+          mod_ssl_test_cases="${docker_repo_root}/mod_ssl/test_cases"
+          cp $mod_ssl_test_cases/* $afl_input
+          afl_instrument_mod_ssl="${docker_repo_root}/mod_ssl/mod_ssl_instrument_w_aflplusplus.sh"
+          afl_instrument_httpd_and_fuzz_session_init="${afl_instrument_mod_ssl} &&"
+          ;;
+        *) echo "Invalid httpd_module ${httpd_module}"
+           echo 'Use -L to list modules supported.'
+           usage;; 
+      esac
+    done
+    afl_instrument_httpd_and_fuzz_session_init="${afl_instrument_httpd_and_fuzz_session_init} ${fuzz_session_init}"
     
     # Instrument & Run Master
     sudo sysctl -w kernel.unprivileged_userns_clone=1
