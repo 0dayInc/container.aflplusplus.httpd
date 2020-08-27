b2d09c2b89ee403624e5d0732e02c30de74e400b
Jacob Hoopes <jake.hoopes@gmail.com>
2020-08-27 15:08:45 -0600

    Attempt to get mod_fastcgi instrumented

--- mod_fastcgi/mod_fastcgi_instrument_w_aflplusplus.sh
+++ mod_fastcgi/mod_fastcgi_instrument_w_aflplusplus.sh
@@ -2,10 +2,8 @@
 # TODO: Get lto working
 #preferred_afl='afl-clang-lto'
 #preferred_aflplusplus='afl-clang-lto++'
-#preferred_afl='afl-clang-fast'
-#preferred_aflplusplus='afl-clang-fast++'
-preferred_afl='afl-gcc'
-preferred_aflplusplus='afl-g++'
+preferred_afl='afl-clang-fast'
+preferred_aflplusplus='afl-clang-fast++'
 
 docker_repo_root='/opt/container.aflplusplus.httpd'
 
@@ -15,19 +13,18 @@ afl_session_root="${fuzz_session_root}/AFLplusplus"
 afl_input="${afl_session_root}/input"
 afl_output="${afl_session_root}/multi_sync"
 
-github_root='https://github.com/FastCGI-Archives/FastCGI.com/raw/master/original_snapshot'
-mod_fastcgi_tar_gz='mod_fastcgi-SNAP-0910052141.tar.gz'
-mod_fastcgi_uri="${github_root}/${mod_fastcgi_tar_gz}"
+fastcgi_github='https://github.com/FastCGI-Archives/mod_fastcgi'
 
 httpd_repo="${fuzz_session_root}/httpd_src"
 httpd_prefix="${fuzz_session_root}/httpd"
-mod_fastcgi_repo="${httpd_repo}/mod_fastcgi-SNAP-0910052141"
+mod_fastcgi_repo="${httpd_repo}/modules/mod_fastcgi"
 repo_name=`basename ${mod_fastcgi_repo}`
 
-cd $httpd_repo && wget $mod_fastcgi_uri && tar -xzvf $mod_fastcgi_tar_gz
+cd `dirname ${mod_fastcgi_repo}`
+git clone $fastcgi_github
 
 # Instrument mod_fastcgi
 cd ${mod_fastcgi_repo}
 cp Makefile.AP2 Makefile
-CC=$preferred_afl CXX=$preferred_aflplusplus make top_dir=$httpd_repo
+CC=$preferred_afl CXX=$preferred_aflplusplus make top_dir=$httpd_prefix
 make install
