69fefdec10f6c91bca76113c4d62fe45eeacc3df
Jacob Hoopes <jake.hoopes@gmail.com>
2020-08-31 11:06:06 -0600

    Locking in instrumentation for now

--- instrumentation_globals.sh
+++ instrumentation_globals.sh
@@ -5,6 +5,6 @@ export AFL_LLVM_INSTRIM_LOOPHEAD=1
 export AFL_LLVM_LTO_AUTODICTIONARY=1
 export AFL_LLVM_LAF_ALL=1
 export AFL_HARDEN=1
-#export AFL_USE_ASAN=1
+#export AFL_USE_ASAN=1 # No Workie w/ afl-clang-fast
 export AFL_USE_UBSAN=1
 export AFL_USE_CFISAN=1
