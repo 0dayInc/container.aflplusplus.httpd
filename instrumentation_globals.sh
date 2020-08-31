26f352acf40c3a31fba83cfe3813c8aa6f503afd
Jacob Hoopes <jake.hoopes@gmail.com>
2020-08-31 11:09:21 -0600

    Attempt to enable MSAN && begin determining if UBSAN OR CFISAN is breaking stuff

--- instrumentation_globals.sh
+++ instrumentation_globals.sh
@@ -6,5 +6,6 @@ export AFL_LLVM_LTO_AUTODICTIONARY=1
 export AFL_LLVM_LAF_ALL=1
 export AFL_HARDEN=1
 #export AFL_USE_ASAN=1 # No Workie w/ afl-clang-fast
-export AFL_USE_UBSAN=1
+#export AFL_USE_MSAN=1
+#export AFL_USE_UBSAN=1
 export AFL_USE_CFISAN=1
