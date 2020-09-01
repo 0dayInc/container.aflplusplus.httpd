c77da376f22557c1e39e5c3dc4f00706cc072513
Jacob Hoopes <jake.hoopes@gmail.com>
2020-09-01 16:18:39 -0600

    Add some performance tweaks to the OS

--- instrumentation_globals.sh
+++ instrumentation_globals.sh
@@ -10,7 +10,7 @@ export preferred_afl_nm='llvm-nm-11'
 
 export AFL_LLVM_MAP_DYNAMIC=1
 export AFL_LLVM_LTO_AUTODICTIONARY=1
-export AFL_LLVM_INSTRUMENT=CFG
+export AFL_LLVM_INSTRUMENT=CFG,CTX
 export AFL_LLVM_INSTRIM_LOOPHEAD=1
 
 #export AFL_LLVM_LAF_SPLIT_SWITCHES=1
@@ -18,10 +18,17 @@ export AFL_LLVM_INSTRIM_LOOPHEAD=1
 #export AFL_LLVM_LAF_SPLIT_FLOATS=1
 #export AFL_LLVM_LAF_ALL=1
 #export AFL_HARDEN=1
+
+# Use Address Sanitizer
 #export AFL_USE_ASAN=1
 #export ASAN_OPTIONS=verbosity=3,detect_leaks=0,abort_on_error=1,symbolize=0,check_initialization_order=true,detect_stack_use_after_return=true,strict_string_checks=true,detect_invalid_pointer_pairs=2 
 
+# Use Memory Sanitizer
 export AFL_USE_MSAN=1
 #export MSAN_OPTIONS=exit_code=86,abort_on_error=1,symbolize=0,msan_track_origins=0,allocator_may_return_null=1
+
+# Use Unexpected Behavior Sanitizer
 #export AFL_USE_UBSAN=1
+
+# Use Control Flow Integrity Sanitizer
 #export AFL_USE_CFISAN=1
