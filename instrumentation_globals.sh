308ee0f9a81eff0e27d47259970db8e2d1138367
Jacob Hoopes <jake.hoopes@gmail.com>
2020-09-01 07:18:01 -0600

    Disable UBSAN for now and test only CFISAN

--- instrumentation_globals.sh
+++ instrumentation_globals.sh
@@ -22,5 +22,5 @@ export AFL_LLVM_INSTRIM_LOOPHEAD=1
 #export ASAN_OPTIONS=verbosity=3,detect_leaks=0,abort_on_error=1,symbolize=0,check_initialization_order=true,detect_stack_use_after_return=true,strict_string_checks=true,detect_invalid_pointer_pairs=2 
 export AFL_USE_MSAN=1
 #export MSAN_OPTIONS=exit_code=86,abort_on_error=1,symbolize=0,msan_track_origins=0,allocator_may_return_null=1
-export AFL_USE_UBSAN=1
+#export AFL_USE_UBSAN=1
 export AFL_USE_CFISAN=1
